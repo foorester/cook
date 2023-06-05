ed0ac6fa5f13a6867de186811df2a867e28f1758
Adrian K <me@adrianpk.com>
2023-06-05 21:50:40 +0200

    Save recipe bare implementation

--- internal/core/service/recipe.go
+++ internal/core/service/recipe.go
@@ -6,16 +6,18 @@ import (
 	"github.com/foorester/cook/internal/core/model"
 	"github.com/foorester/cook/internal/core/port"
 	"github.com/foorester/cook/internal/sys"
+	"github.com/foorester/cook/internal/sys/errors"
 )
 
 type (
 	RecipeService interface {
 		sys.Worker
+		Repo() port.RecipeRepo
 		SaveRecipe(ctx context.Context, r model.Recipe) error
 	}
 
 	Recipe struct {
-		sys.Worker
+		*sys.BaseWorker
 		repo   port.RecipeRepo
 		mailer port.Mailer
 	}
@@ -27,12 +29,25 @@ const (
 
 func NewService(rr port.RecipeRepo, opts ...sys.Option) *Recipe {
 	return &Recipe{
-		Worker: sys.NewWorker(name, opts...),
-		repo:   rr,
-		mailer: nil, // Interface not implemented yet
+		BaseWorker: sys.NewWorker(name, opts...),
+		repo:       rr,
+		mailer:     nil, // Interface not implemented yet
 	}
 }
 
 func (rs *Recipe) SaveRecipe(ctx context.Context, r model.Recipe) error {
-	panic("not implemented yet")
+	// Validate model
+
+	// Persist it
+	err := rs.Repo().Save(ctx, r)
+	if err != nil {
+		return errors.Wrap("error saving recipe", err)
+	}
+
+	// Send a message to bus
+	return nil
+}
+
+func (rs *Recipe) Repo() port.RecipeRepo {
+	return rs.repo
 }
---
+++ internal/infra/repo/pg/tx.go
@@ -0,0 +1,29 @@
+package pg
+
+import (
+	"context"
+	"errors"
+
+	"github.com/foorester/cook/internal/infra/db"
+)
+
+type (
+	ctxKey string
+)
+
+const (
+	TxKey = ctxKey("tx")
+)
+
+var (
+	TxNotFoundError = errors.New("no transaction found in context")
+)
+
+func (rr *RecipeRepo) Tx(ctx context.Context) (db.Tx, error) {
+	tx, ok := ctx.Value(TxKey).(db.Tx)
+	if !ok {
+		return nil, TxNotFoundError
+	}
+
+	return tx, nil
+}
