4d152bcbe8095a202b23801f7652f68097b61afb
Adrian K <adrian.git@adrianpk.com>
2023-06-14 12:28:11 +0200

    Naming updated

--- internal/sys/log/log.go
+++ internal/sys/log/log.go
@@ -27,19 +27,19 @@ type Logger interface {
 }
 
 type SimpleLogger struct {
-	debugLogger *log.Logger
-	infoLogger  *log.Logger
-	errorLogger *log.Logger
-	logLevel    LogLevel
+	debug    *log.Logger
+	info     *log.Logger
+	error    *log.Logger
+	logLevel LogLevel
 }
 
 func NewLogger(logLevel string) *SimpleLogger {
 	level := toValidLevel(logLevel)
 	return &SimpleLogger{
-		debugLogger: log.New(os.Stdout, "[DBG] ", log.LstdFlags),
-		infoLogger:  log.New(os.Stdout, "[INF] ", log.LstdFlags),
-		errorLogger: log.New(os.Stderr, "[ERR] ", log.LstdFlags),
-		logLevel:    level,
+		debug:    log.New(os.Stdout, "[DBG] ", log.LstdFlags),
+		info:     log.New(os.Stdout, "[INF] ", log.LstdFlags),
+		error:    log.New(os.Stderr, "[ERR] ", log.LstdFlags),
+		logLevel: level,
 	}
 }
 
@@ -49,40 +49,40 @@ func (l *SimpleLogger) SetLogLevel(level LogLevel) {
 
 func (l *SimpleLogger) Debug(v ...any) {
 	if l.logLevel <= Debug {
-		l.debugLogger.Println(v...)
+		l.debug.Println(v...)
 	}
 }
 
 func (l *SimpleLogger) Debugf(format string, a ...any) {
 	if l.logLevel <= Debug {
 		message := fmt.Sprintf(format, a...)
-		l.debugLogger.Println(message)
+		l.debug.Println(message)
 	}
 }
 
 func (l *SimpleLogger) Info(v ...any) {
 	if l.logLevel <= Info {
-		l.infoLogger.Println(v...)
+		l.info.Println(v...)
 	}
 }
 
 func (l *SimpleLogger) Infof(format string, a ...any) {
 	if l.logLevel <= Info {
 		message := fmt.Sprintf(format, a...)
-		l.infoLogger.Println(message)
+		l.info.Println(message)
 	}
 }
 
 func (l *SimpleLogger) Error(v ...any) {
 	if l.logLevel <= Error {
-		l.errorLogger.Println(v...)
+		l.error.Println(v...)
 	}
 }
 
 func (l *SimpleLogger) Errorf(format string, a ...any) {
 	if l.logLevel <= Error {
 		message := fmt.Sprintf(format, a...)
-		l.errorLogger.Println(message)
+		l.error.Println(message)
 	}
 }
 
@@ -104,17 +104,17 @@ func toValidLevel(level string) LogLevel {
 // SetDebugOutput set the internal logger.
 // Used for package testing.
 func (sl *SimpleLogger) SetDebugOutput(debug *bytes.Buffer) {
-	sl.debugLogger = log.New(debug, "", 0)
+	sl.debug = log.New(debug, "", 0)
 }
 
 // SetInfoOutput set the internal logger.
 // Used for package testing.
 func (sl *SimpleLogger) SetInfoOutput(info *bytes.Buffer) {
-	sl.infoLogger = log.New(info, "", 0)
+	sl.info = log.New(info, "", 0)
 }
 
 // SetErrorOutput set the internal logger.
 // Used for package testing.
 func (sl *SimpleLogger) SetErrorOutput(error *bytes.Buffer) {
-	sl.errorLogger = log.New(error, "", 0)
+	sl.error = log.New(error, "", 0)
 }
