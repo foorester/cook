041eb0fe270878488e51d1b43f94751a3415059b
Adrian K <adrian.git@adrianpk.com>
2023-06-24 09:32:05 +0200

    Config keys catalog

---
+++ internal/sys/config/values.go
@@ -0,0 +1,57 @@
+package config
+
+var Key = newCfgKeyReg()
+
+func newCfgKeyReg() *cfgKeyReg {
+	return &cfgKeyReg{
+		// API Server
+
+		APIServerHost:     "http.api.server.host",
+		APIServerPort:     "http.api.server.port",
+		APIServerTimeout:  "http.api.server.shutdown.timeout.secs",
+		APIErrorExposeInt: "api.errors.expose.internal",
+
+		// Postgres
+
+		PgUser:   "db.pg.user",
+		PgPass:   "db.pg.pass",
+		PgDB:     "db.pg.database",
+		PgHost:   "db.pg.host",
+		PgPort:   "db.pg.port",
+		PgSchema: "db.pg.schema",
+		PgSSL:    "db.pg.sslmode",
+
+		// Mongo
+
+		MongoUser: "db.mongo.user",
+		MongoPass: "db.mongo.pass",
+		MongoDB:   "db.mongo.database",
+		MongoHost: "db.mongo.host",
+		MongoPort: "db.mongo.port",
+	}
+}
+
+type cfgKeyReg struct {
+	APIServerHost     string
+	APIServerPort     string
+	APIServerTimeout  string
+	APIErrorExposeInt string
+
+	// Postgres
+
+	PgUser   string
+	PgPass   string
+	PgDB     string
+	PgHost   string
+	PgPort   string
+	PgSchema string
+	PgSSL    string
+
+	// Mongo
+
+	MongoUser string
+	MongoPass string
+	MongoDB   string
+	MongoHost string
+	MongoPort string
+}
